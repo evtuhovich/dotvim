e984236baf71fc72eaf3c2058490bebce6cc9d5c
Ivan Evtukhovich <evtuhovich@gmail.com>
2023-03-14 13:36:42 +0300

    Add terminal to vim

--- init.lua
+++ init.lua
@@ -97,6 +97,7 @@ require('lazy').setup({
     dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
   },
 
+  {'akinsho/toggleterm.nvim', version = "*", config = true},
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim', opts = {} },
   { -- Adds git releated signs to the gutter, as well as utilities for managing changes
