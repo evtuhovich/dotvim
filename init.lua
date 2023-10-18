0a329b7901903afa6e3bad78ec80fa6e3a6665fd
Ivan Evtukhovich <evtuhovich@gmail.com>
2023-10-18 14:31:35 +0300

    Add gitlab plugin for review

--- init.lua
+++ init.lua
@@ -73,6 +73,67 @@ require('lazy').setup({
   'tpope/vim-sleuth',
   'epwalsh/obsidian.nvim',
 
+  {
+    "harrisoncramer/gitlab.nvim",
+    dependencies = {
+      'sindrets/diffview.nvim',
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim", -- Recommended but not required. Better UI for pickers.
+      enabled = true,
+    },
+    build = function() require("gitlab.server").build(true) end,  -- Builds the Go binary
+    config = function()
+      require("gitlab").setup({
+        reviewer = "diffview",
+        popup = { -- The popup for comment creation, editing, and replying
+          exit = "<Esc>",
+          perform_action = "<leader>s", -- Once in normal mode, does action (like saving comment or editing description, etc)
+          perform_linewise_action = "<leader>l", -- Once in normal mode, does the linewise action (see logs for this job, etc)
+        },
+        discussion_tree = { -- The discussion tree that holds all comments
+          blacklist = {}, -- List of usernames to remove from tree (bots, CI, etc)
+          jump_to_file = "o", -- Jump to comment location in file
+          jump_to_reviewer = "m", -- Jump to the location in the reviewer window
+          edit_comment = "e", -- Edit coment
+          delete_comment = "dd", -- Delete comment
+          reply = "r", -- Reply to comment
+          toggle_node = "t", -- Opens or closes the discussion
+          toggle_resolved = "p", -- Toggles the resolved status of the discussion
+          position = "left", -- "top", "right", "bottom" or "left"
+          size = "20%", -- Size of split
+          relative = "editor", -- Position of tree split relative to "editor" or "window"
+          resolved = '✓', -- Symbol to show next to resolved discussions
+          unresolved = '✖', -- Symbol to show next to unresolved discussions
+        },
+        review_pane = { -- Specific settings for different reviewers
+          delta = {
+            added_file = "", -- The symbol to show next to added files
+            modified_file = "", -- The symbol to show next to modified files
+            removed_file = "", -- The symbol to show next to removed files
+          }
+        },
+        dialogue = { -- The confirmation dialogue for deleting comments
+          focus_next = { "j", "<Down>", "<Tab>" },
+          focus_prev = { "k", "<Up>", "<S-Tab>" },
+          close = { "<Esc>", "<C-c>" },
+          submit = { "<CR>", "<Space>" },
+        },
+        pipeline = {
+          created = "",
+          pending = "",
+          preparing = "",
+          scheduled = "",
+          running = "ﰌ",
+          canceled = "ﰸ",
+          skipped = "ﰸ",
+          success = "✓",
+          failed = "",
+        },
+      }) -- Uses delta reviewer by default
+    end,
+  },
+
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   { -- LSP Configuration & Plugins
@@ -325,10 +386,10 @@ vim.o.spelllang = 'ru,en'
 require('neogit').setup {}
 
 require("ibl").setup {
-      indent = {
-        char = '┊',
-      },
-    }
+  indent = {
+    char = '┊',
+  },
+}
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
